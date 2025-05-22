b430e97b1e973636dcef2650ed43775da0d06852
Mariah Hoffmann <mariah.hoffmann@gmail.com>
2025-05-21 22:15:29 -0300

    arrumando responsividade

---
+++ public/server.js
@@ -0,0 +1,19 @@
+import cors from "cors";
+
+
+const allowedOrigins = [
+  "http://127.0.0.1:5500",    // dev local
+  "https://adopets-7b387y4jy-mariahs-projects-e924f2e3.vercel.app"  // front em produção
+];
+
+app.use(cors({
+  origin: function(origin, callback) {
+    // permitir requests sem origin (Postman, por exemplo) ou os que estiverem na lista
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("CORS não permitido para origem " + origin));
+    }
+  },
+  credentials: true
+}));
