f2c54592eed6d70942442c5bf9a865a06bbe1e33
Mariah Hoffmann <mariah.hoffmann@gmail.com>
2025-08-11 11:53:10 -0300

    corrigindo bug redirecionamento pagina

---
+++ public/JS/utils.js
@@ -0,0 +1,59 @@
+import { initMenuHamburguer } from './header.js';
+
+
+export function getAssetBasePath() {
+    const path = window.location.pathname;
+    if (path.includes('pages')) {
+      return "../../assets";  
+    } else {
+      return "./assets";  
+    }
+  }
+  
+  export function loadHeaderFooter(headerPath, footerPath, headerContainerId, footerContainerId) {
+    fetch(headerPath)
+      .then(res => res.text())
+      .then((data) => {
+        const basePath = getAssetBasePath();
+        const updatedHeader = data.replace(/{{path_to_assets}}/g, basePath); 
+        document.getElementById(headerContainerId).innerHTML = updatedHeader;
+  
+        const path = window.location.pathname;
+        const linkInicio = document.getElementById("link-inicio");
+        const logoLink = document.getElementById("logo-link");
+  
+        if (path.includes("index.html") || path === "/" || path.endsWith("/index")) {
+          linkInicio.href = "#home";
+          logoLink.href = "#home";
+        } else {
+          linkInicio.href = "../../index.html";
+          logoLink.href = "../../index.html";
+        }
+  
+        initMenuHamburguer();
+      });
+  
+    fetch(footerPath)
+      .then(res => res.text())
+      .then((data) => {
+        const basePath = getAssetBasePath();
+        const updatedFooter = data.replace(/{{path_to_assets}}/g, basePath); 
+        document.getElementById(footerContainerId).innerHTML = updatedFooter;
+      });
+
+
+  }
+  
+  export function preventRedirectionToSamePage(linkElement) {
+    linkElement.addEventListener('click', function(event) {
+      const currentPath = window.location.pathname.split("/").slice(-1)[0].split("#")[0];
+      console.log("Página atual:", currentPath);
+      const targetPage = linkElement.getAttribute("href").split("/").slice(-1)[0].split("#")[0];
+      console.log("Página de destino:", targetPage);
+      if (currentPath === targetPage) {
+        event.preventDefault();  // Impede o redirecionamento
+        console.log("Você já está na página " + targetPage);
+        window.scrollTo(0, 0);  // Rolagem até o topo (opcional)
+      }
+    });
+  }
